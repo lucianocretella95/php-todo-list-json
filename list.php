addd102ccdf4d28c7b71cefc9c0c1908e34761bb
lucianocretella95 <luciano.cretella@tim.it>
2024-06-14 16:43:01 +0200

    M-1

--- list.php
+++ list.php
@@ -1,17 +1,5 @@
 <?php
-
-// $list = [
-// 'spesa',
-// 'meccanico',
-// 'lavanderia',
-// 'barbiere',
-// 'andare in banca'
-// ];
-// header('Content-Type: application/json');
-
-// echo json_encode($list);
-
-$fileContent = file_get_contents("list.json");
+$fileContent = file_get_contents("todo.json");
 
 header('Content-Type: application/json');
 
--- script.js
+++ script.js
@@ -4,13 +4,18 @@ createApp({
   data() {
     return {
       list: [],
+      postRequestConfig: {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      },
     };
   },
   methods: {},
   mounted() {
-    axios.get("./server.php").then((results) => {
-      // console.log("Risultati: ", results);
+    axios.get("./list.php").then((results) => {
       this.list = results.data;
+      console.log(results.data);
     });
   },
 }).mount("#app");
